76326e9105d833d29061b5a5133abb68bfeeb9b7
Elliott Lovell <elliott.lovell88@gmail.com>
2024-11-25 16:23:14 +1100

    x

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,7 +1,6 @@
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
-import { userTable } from "@/server/db/schema";
 
 export const postRouter = createTRPCRouter({
   hello: publicProcedure
