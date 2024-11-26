0af4f82f565aaebca831fb08bccdf337c562fd69
Elliott Lovell <elliott.lovell88@gmail.com>
2024-11-26 20:37:53 +1100

    working

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,27 +1,17 @@
 import "@/styles/globals.css";
 
-import { ClerkProvider } from "@clerk/nextjs";
-import { GeistSans } from "geist/font/sans";
-
 import { LayoutSidebar } from "@/components/navbar/layout-sidebar";
 import { SidebarProvider } from "@/components/ui/sidebar";
-import { TRPCReactProvider } from "@/trpc/react";
-
+import { SidebarRightProvider } from "@/components/ui/sidebar-right";
 
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${GeistSans.variable}`}>
-      <body>
-        <ClerkProvider>
-          <TRPCReactProvider>
-            <SidebarProvider>
-              <LayoutSidebar>{children}</LayoutSidebar>
-            </SidebarProvider>
-          </TRPCReactProvider>
-        </ClerkProvider>
-      </body>
-    </html>
+    <SidebarRightProvider>
+      <SidebarProvider>
+        <LayoutSidebar>{children}</LayoutSidebar>
+      </SidebarProvider>
+    </SidebarRightProvider>
   );
 }
