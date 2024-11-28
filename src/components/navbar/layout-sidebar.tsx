d155899c7909a56861699e91768bc261b2b3bff4
Elliott Lovell <elliott.lovell88@gmail.com>
2024-11-28 19:00:49 +1100

    x

--- src/components/navbar/layout-sidebar.tsx
+++ src/components/navbar/layout-sidebar.tsx
@@ -12,7 +12,7 @@ export function LayoutSidebar({ children }: { children: React.ReactNode }) {
       <SidebarInset className="z-10">
         <header className="fixed z-10 flex h-16 shrink-0 items-center justify-between transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
           <div className="flex items-center gap-2 rounded-lg px-4">
-            <SidebarTrigger className="-ml-1" />
+            <SidebarTrigger className="-ml-1 bg-background p-4" />
             <PropertySwitcher />
             {/* <Separator orientation="vertical" className="mr-2 h-4" />
             <Breadcrumb>
