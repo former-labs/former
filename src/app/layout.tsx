f8e552d34acffaaf87d73a23c023b5a8703ed225
Elliott Lovell <elliott.lovell88@gmail.com>
2024-11-28 17:50:22 +1100

    gtm only for production

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,31 +20,39 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
       <head>
-        {/* Google Tag Manager */}
-        <Script
-          id="google-tag-manager"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        {process.env.NODE_ENV === "production" && (
+          <>
+            {/* Google Tag Manager */}
+            <Script
+              id="google-tag-manager"
+              strategy="afterInteractive"
+              dangerouslySetInnerHTML={{
+                __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-T4Q55MB4');`,
-          }}
-        />
-        {/* End Google Tag Manager */}
+              }}
+            />
+            {/* End Google Tag Manager */}
+          </>
+        )}
       </head>
       <body>
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-T4Q55MB4"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
-        {/* End Google Tag Manager (noscript) */}
+        {process.env.NODE_ENV === "production" && (
+          <>
+            {/* Google Tag Manager (noscript) */}
+            <noscript>
+              <iframe
+                src="https://www.googletagmanager.com/ns.html?id=GTM-T4Q55MB4"
+                height="0"
+                width="0"
+                style={{ display: "none", visibility: "hidden" }}
+              />
+            </noscript>
+            {/* End Google Tag Manager (noscript) */}
+          </>
+        )}
         <TRPCReactProvider>
           <AuthProvider>
             <GoogleAnalyticsProvider>{children}</GoogleAnalyticsProvider>
