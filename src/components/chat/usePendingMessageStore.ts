1ca403b5b00ae08d08addb83e115da61fbbb22bf
Matty Hempstead <matty.hempstead@gmail.com>
2024-11-29 12:58:41 +1100

    Add new item to dashboard

--- src/components/chat/usePendingMessageStore.ts
+++ src/components/chat/usePendingMessageStore.ts
@@ -2,6 +2,12 @@
 
 import { create } from "zustand";
 
+/**
+ * This store is used to temporarily store a user message before a response from the
+ * server is received.
+ * 
+ * This is how we do optimistic updates.
+ */
 export const usePendingMessageStore = create<{
   pendingUserMessage: string;
   setPendingUserMessage: (message: string) => void;
