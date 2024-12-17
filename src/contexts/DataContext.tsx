d393ee6764a92d185012b6867b1fd27bcb2f8ee1
Matty Hempstead <matty.hempstead@gmail.com>
2024-12-18 09:03:48 +1100

    Add back schema

--- src/contexts/DataContext.tsx
+++ src/contexts/DataContext.tsx
@@ -2,6 +2,7 @@
 
 import { type Driver } from "@/electron/drivers/clients";
 import {
+  databaseMetadataSchema,
   type DatabaseConfig,
   type DatabaseCredentials,
   type DatabaseMetadata,
@@ -158,8 +159,8 @@ export const DataProvider = ({ children }: { children: React.ReactNode }) => {
       // I'm validating here because unsafe casting occurs deep in this funciton
       // Imo we should push this validation deeper into electron itself
       const metadata = await window.electron.database.getMetadata(connectionId);
-      // const validatedMetadata = databaseMetadataSchema.parse(metadata);
-      setDatabaseMetadata(metadata);
+      const validatedMetadata = databaseMetadataSchema.parse(metadata);
+      setDatabaseMetadata(validatedMetadata);
       console.log("metadata", metadata);
     } catch (error) {
       console.error("Error fetching metadata:", error);
