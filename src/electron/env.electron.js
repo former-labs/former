ea46faf8b6f4013d955391c9e26ce32f4a4a4e4c
Elliott Lovell <elliott.lovell88@gmail.com>
2025-01-15 20:23:08 +1100

    x

--- src/electron/env.electron.js
+++ src/electron/env.electron.js
@@ -21,16 +21,23 @@ const envSchema = z.object({
   DASHBOARD_URI: z.string(),
 });
 
-// const envDevelopment = {
+// const envLocal = {
 //   NODE_ENV: "development",
 //   DASHBOARD_URI: "http://localhost:3000",
 // };
 
-// export const env = envSchema.parse(envDevelopment);
+// export const env = envSchema.parse(envLocal);
 
-const envProduction = {
-  NODE_ENV: "production",
-  DASHBOARD_URI: "https://app.formerlabs.com",
+const envDevelopment = {
+  NODE_ENV: "development",
+  DASHBOARD_URI: "https://dev.app.formerlabs.com",
 };
 
-export const env = envSchema.parse(envProduction);
+export const env = envSchema.parse(envDevelopment);
+
+// const envProduction = {
+//   NODE_ENV: "production",
+//   DASHBOARD_URI: "https://app.formerlabs.com",
+// };
+
+// export const env = envSchema.parse(envProduction);
