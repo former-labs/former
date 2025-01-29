059d3566c358bdb918c9fe217f1f2228092f00e1
Matty Hempstead <matty.hempstead@gmail.com>
2025-01-30 10:04:23 +1100

    Bump to 0.2.3

--- src/electron/env.electron.js
+++ src/electron/env.electron.js
@@ -37,8 +37,7 @@ export const env = envSchema.parse(envLocal);
 
 // const envProduction = {
 //   NODE_ENV: "production",
-//   DASHBOARD_URI:
-//     "https://desktop.formerlabs.com?_vercel_share=xOJj7LJ0KbQgyt5InDXNwF9YCN0lsgoa",
+//   DASHBOARD_URI: "https://desktop.formerlabs.com",
 // };
 
 // export const env = envSchema.parse(envProduction);
