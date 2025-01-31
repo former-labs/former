e2ca479802ef06f1d067225a16a9d11c594d4f58
Matty Hempstead <matty.hempstead@gmail.com>
2025-01-31 19:56:15 +1100

    Fix

--- src/app/(main)/editor/_components/editor/SqlEditor.tsx
+++ src/app/(main)/editor/_components/editor/SqlEditor.tsx
@@ -403,7 +403,7 @@ export const SqlEditor = () => {
       <div className="min-h-0 flex-1 border-t">
         <div className="relative h-full text-sm">
           <div
-            className={`pointer-events-none absolute left-[70px] z-10 font-sans italic text-gray-400 opacity-80 ${!editorContent && inlinePromptWidgets.length === 0 ? "" : "hidden"} `}
+            className={`pointer-events-none absolute left-[70px] z-10 font-sans italic tracking-wide text-gray-400 opacity-80 ${!editorContent && inlinePromptWidgets.length === 0 ? "" : "hidden"} `}
           >
             Use Cmd+K for AI inline or Cmd+L for AI chat...
           </div>
